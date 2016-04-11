47415c4d759bc806256d4698f7d62fe1af3433f8
leekokkuen <84341838@qq.com>
2016-04-12 00:25:03 +0800

    Update HelloGit.java

--- src/kok/lee/hello/HelloGit.java
+++ src/kok/lee/hello/HelloGit.java
@@ -3,7 +3,7 @@ package kok.lee.hello;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub Hello GitHub");
+		System.out.println("Hello GitHub");
 	}
 
 }
